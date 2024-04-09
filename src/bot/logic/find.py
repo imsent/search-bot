970b4e1a569ecc4f182a137c014b389f496a4650
kirikemae <128600909+kirikemae@users.noreply.github.com>
2024-04-09 18:41:46 +0500

    adding search result output

--- src/bot/logic/find.py
+++ src/bot/logic/find.py
@@ -1,13 +1,12 @@
-import asyncio
-
 from aiogram import Router, types
-from aiogram.filters import CommandStart
+from src.bot.Background.QA import search
+
 
 find_router = Router(name='find')
 
 
 @find_router.message()
-async def start_handler(message: types.Message):
+async def find_handler(message: types.Message):
     msg = await message.answer('Здравствуйте! Спасибо за ваш запрос. Я начну поиск контактов по вашему запросу, это может занять некоторое время')
-    await asyncio.sleep(2)
-    await msg.edit_text("По вашему запросу ничего не найдено.")
+    answer = search(message.text)
+    await msg.edit_text("По вашему запросы ничего не найдено. Пожалуйста, проверьте правильность введенной вами информации. Если все введено верно, то попробуйте написать запрос по-другому." if len(answer) == 0 else answer[0])
