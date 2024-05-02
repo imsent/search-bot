d3718870d5a41bdb1186073654f8672d91ca46f6
Николай <91284688+imsent@users.noreply.github.com>
2024-05-02 18:45:49 +0500

    new QA

--- background/QA.py
+++ background/QA.py
@@ -1,10 +1,33 @@
-from background.db import run
-import spacy
+from langchain_community.llms import Ollama
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.chains import RetrievalQA
+import json
 
-nlp = spacy.load("ru_core_news_lg")
+model = Ollama(model="mistral:instruct")
+with open('employees_info.json', 'r') as f:
+    data = json.load(f)
+
+content = []
+for industry, departments in data.items():
+    for department, employees in departments.items():
+        for employee, info in employees.items():
+            text = f"{employee} работает в отделе {department}, подразделения {industry}, занимает должность {info['Должность']} и выполняет функции:"
+            for function in info['Функции']:
+                text += f" {function},"
+            text = text[:-1] + '.'
+            content.append(text)
+chunking = content
+embeddings = OllamaEmbeddings(model="mistral:instruct")
+vectorstore = FAISS.from_texts(chunking, embeddings)
+retriever = vectorstore.as_retriever(search_type='similarity', search_kwargs={'k': 5})
+qa = RetrievalQA.from_chain_type(
+    llm=model,
+    retriever=retriever,
+    return_source_documents=True,
+)
 
 
 async def search(query):
-    query_embedding = nlp(query).vector.tolist()
-    result = await run(query_embedding)
-    return result
+    responses = qa.invoke({"query": query})
+    return [x.page_content for x in responses['source_documents']]
