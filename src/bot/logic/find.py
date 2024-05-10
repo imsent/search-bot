78053e487147418f447b37d41cfccd02b7962f5c
kirikemae <128600909+kirikemae@users.noreply.github.com>
2024-05-10 22:02:16 +0500

    find

--- src/bot/logic/find.py
+++ src/bot/logic/find.py
@@ -1,12 +1,26 @@
 from aiogram import Router, types
-from src.bot.Background.QA import search
-
+import aiohttp
+from src.configuration import conf
 
 find_router = Router(name='find')
 
 
+async def url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            result = await resp.json()
+            return result
+
+
 @find_router.message()
 async def find_handler(message: types.Message):
-    msg = await message.answer('Здравствуйте! Спасибо за ваш запрос. Я начну поиск контактов по вашему запросу, это может занять некоторое время')
-    answer = search(message.text)
-    await msg.edit_text("По вашему запросы ничего не найдено. Пожалуйста, проверьте правильность введенной вами информации. Если все введено верно, то попробуйте написать запрос по-другому." if len(answer) == 0 else f"Найдено {len(answer)} контактов:\n{'\n'.join(answer)}")
+    msg = await message.answer(
+        "Здравствуйте! Спасибо за ваш запрос. Я начну поиск контактов по вашему запросу, это может занять некоторое время")
+    answer = await url(f"http://{conf.host}:8080/search/{message.text}")
+
+    await msg.edit_text(
+        f"Найдены сотрудники, соответствующие вашему запросу:")
+    for x in answer['answer']:
+        await message.answer(f'{x['name']}\n✉️ {x["mail"]}\nТел: {x["number"]}\nКомпания: {x["company"]}\nДолжность: {x["post"]}')
+
+
