cbe65276e50a871c8b44e3a84cff2c07f4ebc607
Николай <91284688+imsent@users.noreply.github.com>
2024-05-17 21:27:52 +0500

    add prompt

--- background/QA.py
+++ background/QA.py
@@ -1,18 +1,41 @@
 from langchain_community.llms import Ollama
-from langchain.chains import RetrievalQA
 from background.db import vectorstore
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+import re
 
-model = Ollama(model="mistral:instruct")
+prompt = ChatPromptTemplate.from_template("""Перечисли ID сотрудников, которые подходят для ответа на вопрос. Если недостаточно информации для выбора сотрудника, напиши 'Мало информации'. Если вопрос не относится к сотрудникам, напиши 'Нет ответа':
 
-retriever = vectorstore.as_retriever(search_type='similarity', search_kwargs={'k': 5})
-qa = RetrievalQA.from_chain_type(
-    llm=model,
-    retriever=retriever,
-    return_source_documents=True,
-)
+<context>
+{context}
+</context>
+
+Question: {input}""")
+
+model = Ollama(model="mistral:instruct", temperature=0)
+
+retriever = vectorstore.as_retriever(search_kwargs={'k': 10})
+
+document_chain = create_stuff_documents_chain(model, prompt)
+retrieval_chain = create_retrieval_chain(retriever, document_chain)
 
 
 async def search(query):
-    responses = qa.invoke({"query": query})
-    print(responses['result'])
-    return [x.metadata for x in responses['source_documents']]
+    response = retrieval_chain.invoke({"input": query})
+    print(response['answer'])
+    print(response)
+    if 'Нет ответа' in response['answer']:
+        return ['Нет ответа']
+    if 'Мало информации' in response['answer']:
+        return ['Мало информации']
+    h = str(response["answer"])
+    ids = []
+    try:
+        for x in range(h.count("ID")):
+            ids.append(int(re.sub(r"\D", "", h[h.find('ID') + 3:h.find('ID') + 8])))
+            h = h[h.find('ID') + 8:]
+        result = [x for x in response['context'] if x.metadata['id'] in ids]
+    except:
+        result = response['context'][:5]
+    return [x.metadata for x in result]
